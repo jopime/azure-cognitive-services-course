c776d4b250071bd06285727cdcc428c192270338
jopime <jose.pimentel@factum-it.es>
2021-03-23 17:55:11 +0100

    Complete cap 6 https://microsoftlearning.github.io/AI-102-AIEngineer/Instructions/06-translate-text.html

--- 06-translate-text/Python/text-translation/text-translation.py
+++ 06-translate-text/Python/text-translation/text-translation.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import requests, json
 
+
 def main():
     global translator_endpoint
     global cog_key
@@ -24,34 +25,83 @@ def main():
 
             # Detect the language
             language = GetLanguage(text)
-            print('Language:',language)
+            print('Language:', language)
 
             # Translate if not already English
-            if language != 'en':
+            if language != 'es':
                 translation = Translate(text, language)
                 print("\nTranslation:\n{}".format(translation))
-                
+
     except Exception as ex:
         print(ex)
+
 
 def GetLanguage(text):
     # Default language is English
     language = 'en'
 
     # Use the Translator detect function
+    path = '/detect'
+    url = translator_endpoint + path
 
+    # Build the request
+    params = {
+        'api-version': '3.0'
+    }
 
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get language
+    language = response[0]["language"]
     # Return the language
     return language
+
 
 def Translate(text, source_language):
     translation = ''
 
     # Use the Translator translate function
+    path = '/translate'
+    url = translator_endpoint + path
 
+    # Build the request
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': ['es']
+    }
 
+    headers = {
+        'Ocp-Apim-Subscription-Key': cog_key,
+        'Ocp-Apim-Subscription-Region': cog_region,
+        'Content-type': 'application/json'
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    # Send the request and get response
+    request = requests.post(url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    # Parse JSON array and get translation
+    translation = response[0]["translations"][0]["text"]
     # Return the translation
     return translation
+
 
 if __name__ == "__main__":
     main()
